37cd620f37e7357818f0dd6e81474b7612e1c572
timnevada <timnevada@gmail.com>
2021-12-07 10:58:30 -0800

    completed create, readAll, readOne, update, and delete

--- datastore/index.js
+++ datastore/index.js
@@ -24,11 +24,6 @@ exports.create = (text, callback) => {
 };
 
 exports.readAll = (callback) => {
-  // fs.readdir(exports.dataDir, (err, data) => {
-  //   console.log(data);
-
-  // });
-
 
   const arr = [];
 
@@ -36,94 +31,92 @@ exports.readAll = (callback) => {
     if (err) {
       callback(err, null);
     } else {
-      for (let i = 1; i < data.length; i++) {
-        let fileName = data[i];
-        let filePath = `${exports.dataDir}/${fileName}`;
-        fs.readFile(filePath, (err, content) => {
-          if (err) {
-            callback(err, null);
-          } else {
-            arr.push({ id: fileName.substring(0, 5), text: String(content) });
-          }
-        });
+      if (data.length === 0) {
+        callback(null, arr);
       }
-      callback(arr, null);
+      if (data[0] === '.DS_Store') {
+        data.shift();
+      }
+      for (let i = 0; i < data.length; i++) {
+        let fileName = data[i].slice(0, -4);
+        arr.push({ id: fileName, text: fileName });
+        if (arr.length === data.length) {
+          callback(null, arr);
+        }
+      }
     }
-
-
-    // for (let i = 1; i < data.length; i++) {
-    //   let fileName = data[i];
-    //   let filePath = `${exports.dataDir}/${fileName}`;
-    //   fs.readFile(filePath, (err, content) => {
-    //     if (err) {
-    //       callback(err, null);
-    //     } else {
-    //       arr.push({ id: fileName.substring(0, 5), text: String(content) });
-    //     }
-    //   });
-    // }
-
-
-    // if (err) {
-    //   callback(err, null);
-    // } else {
-    //   callback(null, arr);
-    // }
   });
-  // // fs.promises.readdir(exports.dataDir)
-  // //   .then(fileNames => {
-  // //     for (let i = 1; i < fileNames.length; i++) {
-  // //       let fileName = fileNames[i];
-  // //       let filePath = `${exports.dataDir}/${fileName}`;
-  // //       fs.promises.readFile(filePath)
-  // //         .then((content) => {
-  // //           arr.push({ id: fileName.substring(0, 5), text: String(content) });
-  // //         });
-  // //     }
-  // //   })
-  // //   .then(() => callback(arr, null));
-
-  // .catch(() => callback(err, null))
-  // if (err) {
-  //   callback(err, null);
-  // } else {
-  //   callback(arr, null);
-  // }
-  // var data = _.map(items, (text, id) => {
-  //   return { id, text };
-  // });
-  // callback(null, data);
-  // [ {id: 0001, text: something}, {id: 0001, text: something}]
 };
+
+//// THE SOLUTION THAT MAKES SENSE BUT SPECRUNNER DOESNT LIKE
+
+//   fs.readdir(exports.dataDir, (err, data) => {
+//     if (err) {
+//       callback(err, null);
+//     } else {
+//       if (data.length === 0) {
+//         callback(null, arr);
+//       }
+//       if (data[0] === '.DS_Store') {
+//         data.shift();
+//       }
+//       for (let i = 0; i < data.length; i++) {
+//         let fileName = data[i];
+//         let filePath = `${exports.dataDir}/${fileName}`;
+//         fs.readFile(filePath, 'utf-8', (err, content) => {
+//           if (err) {
+//             callback(err, null);
+//           } else {
+//             arr.push({ id: fileName.slice(0, -4), text: content });
+//             if (data.length === arr.length) {
+//               const orderedArr = arr.sort((a, b) => a.id - b.id);
+//               callback(null, orderedArr);
+//             }
+//           }
+//         });
+//       }
+//     }
+//   });
+// };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  let filePath = `${exports.dataDir}/${id}.txt`;
+  fs.readFile(filePath, 'utf-8', (err, text) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, { id, text });
+    }
+  });
 };
 
+
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  exports.readOne(id, (err, file) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      let filePath = `${exports.dataDir}/${id}.txt`;
+      fs.writeFile(filePath, text, 'utf-8', (err, data) => {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, { id, text });
+        }
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  let filePath = `${exports.dataDir}/${id}.txt`;
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, 'Successfully deleted');
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
